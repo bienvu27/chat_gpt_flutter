a98faa69df7a681055c7d8f9fe8a43e5b1f5fad4
bienvu27 <bienvx1999@gmail.com>
2023-05-27 18:04:43 +0700

    First commit

---
+++ lib/widgets/drop_down.dart
@@ -0,0 +1,51 @@
+import 'package:chat_gpt_flutter/constants/constants.dart';
+import 'package:chat_gpt_flutter/models/models_model.dart';
+import 'package:chat_gpt_flutter/services/api_service.dart';
+import 'package:chat_gpt_flutter/widgets/text_widget.dart';
+import 'package:flutter/material.dart';
+
+class ModelsDropDownWidget extends StatefulWidget {
+  const ModelsDropDownWidget({Key? key}) : super(key: key);
+
+  @override
+  State<ModelsDropDownWidget> createState() => _ModelsDropDownWidgetState();
+}
+
+class _ModelsDropDownWidgetState extends State<ModelsDropDownWidget> {
+  String currentModel = "text-davinci-003";
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder<List<ModelsModel>>(
+        future: ApiService.getModels(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: TextWidget(
+                label: snapshot.error.toString(),
+              ),
+            );
+          }
+          return snapshot.data == null || snapshot.data!.isEmpty
+              ? const SizedBox.shrink()
+              : DropdownButton(
+                  dropdownColor: scaffoldBackgroundColor,
+                  iconEnabledColor: Colors.white,
+                  items: List<DropdownMenuItem<String>>.generate(
+                      snapshot.data!.length,
+                      (index) => DropdownMenuItem(
+                          value: snapshot.data![index].id,
+                          child: TextWidget(
+                            label: snapshot.data![index].id,
+                            fontSize: 15,
+                          ))),
+                  value: currentModel,
+                  onChanged: (value) {
+                    setState(() {
+                      currentModel = value.toString();
+                    });
+                  },
+                );
+        });
+  }
+}
